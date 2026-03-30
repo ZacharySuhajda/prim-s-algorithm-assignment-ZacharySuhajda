a2505e49692a94ab66f5d8068a7b5fd64476727f
ZacharySuhajda <suhajdaz3219@my.uwstout.edu>
2026-03-30 15:52:25 -0500

    Added main, linked compiler, compiled for .exe

--- prim.cpp
+++ prim.cpp
@@ -1,1 +1,6 @@
-//All procedures including main
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Output text to the console
+    return 0; // Indicate successful execution
+}
